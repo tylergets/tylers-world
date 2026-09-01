a81d1181e3cb0a520e07998d1402050db3bf895b
Tyler Getsay <tylergetsay@gmail.com>
2026-08-31 23:08:08 -0400

    Deploy game to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/tylers-world/',
   server: { port: 5173, open: false },
   build: { target: 'es2022', outDir: 'dist' },
 });
